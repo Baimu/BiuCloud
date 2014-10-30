57e5823ad1475c2b161825e32d0a9ad8c62f70bf
彭征新 <pzx@pengzhengxindeMacBook-Pro.local>
2014-10-30 13:37:23 +0800

    3 commit

--- cloud/study.js
+++ cloud/study.js
@@ -1,3 +1,15 @@
-var GameScore = AV.Object.extend("GameScore");
-
-var gameScore = new GameScore();
+AV.Cloud.define("GetEnemy",function(request,response) {
+	var Enemy = AV.Object.extend("Enemy");
+	var enemy = Enemy();
+	var query = new AV.Query(Enemy);
+	var errorMsg = "";
+	query.first({
+ 	 success: function(object) {
+   	 // Successfully retrieved the object.
+  },
+  error: function(error) {
+    errorMsg = "Error: " + error.code + " " + error.message;
+  }
+});
+	response.success("Hello world From ZYB!" + errorMsg);
+});
