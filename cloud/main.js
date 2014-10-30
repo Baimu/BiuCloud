f91ef6d07fcbd949656e5fdbff3447f257860e1a
彭征新 <pzx@pengzhengxindeMacBook-Pro.local>
2014-10-30 13:54:02 +0800

    7 commit

--- cloud/main.js
+++ cloud/main.js
@@ -19,3 +19,19 @@ gameScore.save(null, {
 });
   response.success("Hello world From ZYB!");
 });
+
+AV.Cloud.define("GetEnemy",function(request,response) {
+	var Enemy = AV.Object.extend("Enemy");
+	var enemy = Enemy();
+	var query = new AV.Query(Enemy);
+	var errorMsg = "";
+	query.first({
+ 	 success: function(object) {
+   	 // Successfully retrieved the object.
+  },
+  error: function(error) {
+    errorMsg = "Error: " + error.code + " " + error.message;
+  }
+});
+	response.success("Hello world From ZYB!" + errorMsg);
+});
